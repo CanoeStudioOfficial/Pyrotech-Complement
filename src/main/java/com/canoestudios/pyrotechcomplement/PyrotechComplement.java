6fba5eada2a30c07acc0c26b7bd59b289200b7da
Lonelyxiya <151129412+LonelyxiyaOVO@users.noreply.github.com>
2026-06-28 00:20:03 +0800

    Update PyrotechComplement.java

--- src/main/java/com/canoestudios/pyrotechcomplement/PyrotechComplement.java
+++ src/main/java/com/canoestudios/pyrotechcomplement/PyrotechComplement.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class PyrotechComplement {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
